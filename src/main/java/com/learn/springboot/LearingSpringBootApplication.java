f8693b0e5f35a6805373e190f5235c5dca00d0a4
arpitprajj <arpitprajjwal17361@gmail.com>
2025-04-14 01:08:49 +0530

    Documenting API using Swagger

--- src/main/java/com/learn/springboot/LearingSpringBootApplication.java
+++ src/main/java/com/learn/springboot/LearingSpringBootApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @SpringBootApplication
 public class LearingSpringBootApplication {
